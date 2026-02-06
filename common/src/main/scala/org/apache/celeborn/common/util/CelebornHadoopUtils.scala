bc0f138b9ccf08c2947ddd640f97a33045b7062c
Filip Darmanovic <dzeri96@proton.me>
2026-02-06 16:42:33 +0100

    [CELEBORN-2259] Reverted default S3 credential providers

--- common/src/main/scala/org/apache/celeborn/common/util/CelebornHadoopUtils.scala
+++ common/src/main/scala/org/apache/celeborn/common/util/CelebornHadoopUtils.scala
@@ -63,8 +63,7 @@ object CelebornHadoopUtils extends Logging {
         "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider," +
           "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider," +
           "com.amazonaws.auth.EnvironmentVariableCredentialsProvider," +
-          "org.apache.hadoop.fs.s3a.auth.IAMInstanceCredentialsProvider," +
-          "com.amazonaws.auth.WebIdentityTokenCredentialsProvider")
+          "org.apache.hadoop.fs.s3a.auth.IAMInstanceCredentialsProvider")
 
       hadoopConf.set("fs.s3a.endpoint.region", conf.s3EndpointRegion)
     } else if (conf.ossDir.nonEmpty) {
